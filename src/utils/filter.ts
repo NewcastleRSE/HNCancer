80fd4afbfd634176437bd595f52aecd85240c5ac
rosselton <color.diff>
2026-06-12 16:48:47 +0100

    Initial commit

---
+++ src/utils/filter.ts
@@ -0,0 +1,48 @@
+import type { CSVRow } from '../types';
+
+ /* in the case where 3 query items are joined by /, the male query also greedy matches the female record
+  and the additional result needs to be filtered out */
+
+export function filterArray(data: CSVRow[], query: string){	
+
+	var deleteUnwanted = false;
+	var unwantedFormat = /^Female\/\w+\/?.*$/;
+	// grab the first part of the string
+	var array = query.split('/');
+
+	data.forEach(function (item){
+		if (item.table.match(unwantedFormat) && array[0] === 'male'){
+			deleteUnwanted = true;
+		}
+	})
+
+	if(deleteUnwanted){
+  		data.splice(1,1);
+	}
+	return data;
+ }
+
+ // determines which spreadsheet should be used
+ export function cancerType(value: string){
+
+	var CSV_file = '';
+
+	switch (value) {
+		case "Head and Neck":
+			CSV_file = './HNC.csv';
+			break;
+		case "Laryngeal":
+			CSV_file = './Incidence-Larynx.csv';
+			break;
+		case "Oral Cavity":
+			CSV_file = './Incidence-OralCavity.csv';
+			break;
+		case "Oropharyngeal":
+			CSV_file = './Incidence-Oropharynx.csv';
+			break;
+		default:
+			console.log(`Cancer type not recognised`);
+		}
+
+	return CSV_file;
+ }
