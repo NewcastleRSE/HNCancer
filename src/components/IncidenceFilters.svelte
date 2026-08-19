e6b8b6f8f1a544ed74bc4a4cf83daa440854a86b
Gabrielle Schroeder <8205451+gmschroe@users.noreply.github.com>
2026-08-19 15:40:29 +0100

    Initial Svelte components for query filters (not yet integrated into querying step)

---
+++ src/components/IncidenceFilters.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import CheckboxGroup from "./CheckboxGroup.svelte";
+  import {
+    INCIDENCE_FILTER_VARIABLES,
+    INCIDENCE_FILTER_LABELS,
+    initIncidenceFilter,
+  } from "../utils/query";
+  import { VARIABLE_OPTIONS } from "../utils/variables";
+  import type { IncidenceFilter } from "../types";
+
+  // Initialise variable for storing filter state (updated using UI inputs)
+  let filter: IncidenceFilter = initIncidenceFilter();
+</script>
+
+<div>
+  {#each INCIDENCE_FILTER_VARIABLES as variable}
+    <CheckboxGroup
+      label={INCIDENCE_FILTER_LABELS[variable]}
+      options={VARIABLE_OPTIONS[variable]}
+      bind:selectedValues={filter[variable]}
+    />
+  {/each}
+</div>
